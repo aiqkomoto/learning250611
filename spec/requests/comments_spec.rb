4794ad957d9b4555c3c45cd0667d712f659e7d30
河本 陸生 <rikuo.koumoto@aiqveone.co.jp>
2025-07-25 16:25:10 +0900

    add rspec

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: "テストユーザー", email: "test@example.com") }
+  let(:music) { Music.create!(name: "テスト曲", performer: "テスト演奏者", user: user) }
+
+  before do
+    # current_user を仮に上書き
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  describe "POST /musics/:music_id/comments" do
+    it "コメントを追加できる" do
+      expect {
+        post music_comments_path(music), params: { comment: { content: "素晴らしい曲です！" } }
+      }.to change(Comment, :count).by(1)
+      
+      expect(response).to redirect_to(music_path(music))
+    end
+  end
+
+  describe "DELETE /musics/:music_id/comments/:id" do
+    it "コメントを削除できる" do
+      comment = music.comments.create!(content: "削除対象コメント", user: user)
+
+      expect {
+        delete music_comment_path(music, comment)
+      }.to change(Comment, :count).by(-1)
+
+      expect(response).to redirect_to(music_path(music))
+    end
   end
 end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: "テストユーザー", email: "test@example.com") }
+  let(:music) { Music.create!(name: "テスト曲", performer: "テスト演奏者", user: user) }
+
+  before do
+    # 擬似的にログイン中のユーザーを current_user にする
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  describe "POST /musics/:music_id/like" do
+    it "楽曲にいいねできる" do
+      expect {
+        post music_like_path(music)
+      }.to change(Like, :count).by(1)
+
+      expect(response).to redirect_to(music_path(music))
+    end
+  end
+
+  describe "DELETE /musics/:music_id/like" do
+    before do
+      music.likes.create!(user: user)
+    end
+
+    it "いいねを解除できる" do
+      expect {
+        delete music_like_path(music)
+      }.to change(Like, :count).by(-1)
+
+      expect(response).to redirect_to(music_path(music))
+    end
   end
 end
