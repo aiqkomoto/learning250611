842d9dc65e5f2fb85e77334a92d9d6bb71d09034
河本 陸生 <rikuo.koumoto@aiqveone.co.jp>
2025-09-02 14:59:11 +0900

    authentication_250902

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,22 @@
-class Api::V1::SessionsController < ApplicationController
-  skip_before_action :authenticate_user!
+class Api::V1::SessionsController < Api::BaseController
+  skip_before_action :authenticate_user!, only: :create
 
   def create
-    user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
+    email    = params[:email].to_s
+    password = params[:password].to_s
+
+    user = User.find_by(email: email)
+
+    if user&.authenticate(password)
       token = JsonWebToken.encode(user_id: user.id)
-      render json: { token: token }, status: :ok
+      response.set_header("Authorization", "Bearer #{token}")
+      render json: {
+        token: token,
+        token_type: "Bearer",
+        expires_in: 24.hours.to_i
+      }, status: :ok
     else
-      render json: { error: "認証に失敗しました" }, status: :unauthorized
+      render json: { error: "invalid_email_or_password" }, status: :unauthorized
     end
   end
 end
